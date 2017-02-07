f1c1fb92f7859cf9d76bf2eee90c4651c19d33bf
Paresh Thakor <paresh.thakor@investis.com>
2017-02-07 18:58:45 +0530

    UI Setup

---
+++ RTChatEx/ChannelListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChannelListViewController.swift
+//  RTChatEx
+//
+//  Created by Paresh Thakor on 07/02/17.
+//  Copyright © 2017 Paresh Thakor. All rights reserved.
+//
+
+import Foundation
